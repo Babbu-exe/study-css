e0c66fee4ec391e505b08e9c7004a7a9c06bbb9b
Umar <umarmunafahmed@gmail.com>
2025-12-21 23:15:09 +0530

    swapping two values useing js

---
+++ swaping two var/index.js
@@ -0,0 +1,10 @@
+console.log("Swapping values in JavaScript");
+var a = 5;
+var b = 10;
+console.log("before swapping : a =a",a ,"b=b",b);
+//swapping
+var temp =a;
+a =b;
+b =temp;
+console.log("after swapping : a =",a ,"b =",b);
+    
